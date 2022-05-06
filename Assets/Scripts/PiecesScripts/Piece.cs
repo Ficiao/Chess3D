2aa75b1249773479d47d701507dda2bb3cb266ea
Ficiao <filipmtvn@gmail.com>
2022-05-06 17:20:10 +0200

    Added highlight on mouseover and highlighted path on mosue press

--- Assets/Scripts/PiecesScripts/Piece.cs
+++ Assets/Scripts/PiecesScripts/Piece.cs
@@ -9,29 +9,47 @@ public abstract class Piece : MonoBehaviour
         Black
     }
 
+    [SerializeField]
     private SideColor _pieceColor;
     private Renderer _renderer;
+    private Vector3 _startPosition;
+    public Vector3 StartPosition { get => _startPosition; }
+    private bool active;
     private Color _startColor;
     public SideColor PieceColor { get => PieceColor; }
 
-    public abstract void Move();
-    public abstract void Die();
-    public abstract void Revive();
+    public abstract void CreatePath();
 
     private void Start()
     {
+        _startPosition = transform.position;
+        active = false;
         _renderer = GetComponent<Renderer>();
-        Debug.Log(_renderer.material.color);
         _startColor = _renderer.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        active = true;
+        CreatePath();
     }
 
     private void OnMouseEnter()
     {
         _renderer.material.color = Color.yellow;
     }
+
     private void OnMouseExit()
     {
-        _renderer.material.color = _startColor;
+        if (active == false)
+        {
+            _renderer.material.color = _startColor;
+        }
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
     }
 
 }
--- Assets/Scripts/PiecesScripts/Rook.cs
+++ Assets/Scripts/PiecesScripts/Rook.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class Rook : Piece
 {
-    public override void Die()
+
+
+    public override void CreatePath()
     {
-        throw new System.NotImplementedException();
+        GameObject[] _paths = ObjectPool.Instance.GetHighlightPaths(14);
+        Vector3 _position=new Vector3();
+        int index = 0;
+        for(int i = 0; i < 8; i++)
+        {
+            if (i != (float)(transform.position.x / 1.5))
+            {
+                _position.x = transform.position.x;
+                _position.y = i * 1.5f;
+                _paths[index++].transform.position = _position;
+            }
+
+            if (i != (float)(transform.position.y / 1.5))
+            {
+                _position.x = i * 1.5f;
+                _position.y = transform.position.y;
+                _paths[index++].transform.position = _position;
+            }
+
+        }
     }
 
-    public override void Move()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void Revive()
-    {
-        throw new System.NotImplementedException();
-    }
 }
